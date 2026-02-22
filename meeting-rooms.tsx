d7e20d0a178380d5df58dd0af4081f0f01a90868
Furqan Asghar <furqan.79000@gmail.com>
2026-02-23 02:09:27 +0500

    Refactor code structure for improved readability and maintainability

---
+++ meeting-rooms.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="meeting-rooms" tilewidth="32" tileheight="32" tilecount="180" columns="18">
+ <image source="tilesets/meeting-rooms.png" width="597" height="323"/>
+</tileset>
